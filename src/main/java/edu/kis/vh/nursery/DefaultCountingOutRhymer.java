fb9226de7d9fa2df9ae605311af38d4939f5a507
Czarek <cezaryhanczak@gmail.com>
2022-02-02 11:42:03 +0100

    2.1. Wyłuskanie implementacji struktury stosu

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,37 +1,33 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
+    IntArrayStack intArrayStack;
 
-    public static final int SIZE = 11;
-    public static final int EMPTY = -1;
-    public static final int REAL_SIZE = SIZE + 1;
-    private final int[] numbers = new int[REAL_SIZE];
+    public DefaultCountingOutRhymer() {
+        this.intArrayStack = new IntArrayStack();
+    }
 
-    private int total = EMPTY;
+    public DefaultCountingOutRhymer(IntArrayStack intArrayStack) {
+        this.intArrayStack = intArrayStack;
+    }
 
     protected void countIn(int in) {
-        if (!isFull())
-            numbers[++total] = in;
+        intArrayStack.countIn(in);
     }
 
-
-    protected boolean callCheck() {
-        return total == EMPTY;
+    public boolean callCheck() {
+        return intArrayStack.callCheck();
     }
 
-    protected boolean isFull() {
-        return total == SIZE;
+    public boolean isFull() {
+        return intArrayStack.isFull();
     }
 
-    protected int peekaboo() {
-        if (callCheck())
-            return EMPTY;
-        return numbers[total];
+    public int peekaboo() {
+        return intArrayStack.peekaboo();
     }
 
     protected int countOut() {
-        if (callCheck())
-            return EMPTY;
-        return numbers[total--];
+        return intArrayStack.countOut();
     }
 }
---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,36 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+    public static final int SIZE = 11;
+    public static final int EMPTY = -1;
+    public static final int REAL_SIZE = SIZE + 1;
+    private final int[] numbers = new int[REAL_SIZE];
+
+    private int total = EMPTY;
+
+    protected void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+
+    protected boolean callCheck() {
+        return total == EMPTY;
+    }
+
+    protected boolean isFull() {
+        return total == SIZE;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return EMPTY;
+        return numbers[total];
+    }
+
+    protected int countOut() {
+        if (callCheck())
+            return EMPTY;
+        return numbers[total--];
+    }
+}
